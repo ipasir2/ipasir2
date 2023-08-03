78baae0b2e913f98c43bb3481300e6dfe992dd9c
Markus Iser <markus.iser@kit.edu>
2023-08-03 11:33:12 +0300

    build system: fetch and build ipasir2-solvers; link test-apps

---
+++ load_solvers.cmake
@@ -0,0 +1,75 @@
+
+function(load_cadical)
+    set(CADICAL_DIR ${CMAKE_CURRENT_BINARY_DIR}/solvers/src/cadical_external/build)
+    set(CADICAL_LIB ${CADICAL_DIR}/libcadical.a)
+    message(STATUS "Cadical Library: ${CADICAL_LIB}")
+    if (EXISTS ${CADICAL_LIB})
+        message(STATUS "Cadical Found")
+        add_library(cadical STATIC IMPORTED)
+        set_target_properties(cadical PROPERTIES IMPORTED_LOCATION "${CADICAL_LIB}")
+    else()
+        message(STATUS "Injecting External Project for Fetching and Building Cadical")
+        ExternalProject_Add(cadical_external
+            #GIT_REPOSITORY git@github.com:arminbiere/cadical.git 
+            GIT_REPOSITORY git@github.com:ipasir2/cadical.git
+            GIT_TAG master
+            PREFIX solvers
+            CONFIGURE_COMMAND ./configure
+            BUILD_COMMAND make -j
+            INSTALL_COMMAND ""
+            BUILD_BYPRODUCTS "${CADICAL_LIB}"
+            BUILD_IN_SOURCE 1
+        )
+        add_library(cadical STATIC IMPORTED)
+        add_dependencies(cadical cadical_external)
+        set_target_properties(cadical PROPERTIES IMPORTED_LOCATION "${CADICAL_LIB}")
+    endif()
+endfunction()
+
+function(load_minisat)
+    set(MINISAT_DIR ${CMAKE_CURRENT_BINARY_DIR}/solvers/src/minisat_external)
+    set(MINISAT_LIB "${MINISAT_DIR}-build/libminisat.a")
+    message(STATUS "Minisat Library: ${MINISAT_LIB}")
+    if (EXISTS ${MINISAT_LIB})
+        message(STATUS "Minisat Found")
+        add_library(minisat STATIC IMPORTED)
+        set_target_properties(minisat PROPERTIES IMPORTED_LOCATION "${MINISAT_LIB}")
+    else()
+        message(STATUS "Injecting External Project for Fetching and Building Minisat")
+        ExternalProject_Add(minisat_external
+            #GIT_REPOSITORY git@github.com:Udopia/minisat.git 
+            GIT_REPOSITORY git@github.com:ipasir2/minisat.git
+            GIT_TAG master
+            PREFIX solvers
+            INSTALL_COMMAND ""
+            BUILD_BYPRODUCTS "${MINISAT_LIB}"
+        )
+        add_library(minisat STATIC IMPORTED)
+        add_dependencies(minisat minisat_external)
+        set_target_properties(minisat PROPERTIES IMPORTED_LOCATION "${MINISAT_LIB}")
+    endif()
+endfunction()
+
+function(load_cms)
+    set(CRYPTOMINISAT_DIR ${CMAKE_CURRENT_BINARY_DIR}/solvers/src/cms_external)
+    set(CRYPTOMINISAT_LIB "${CRYPTOMINISAT_DIR}-build/lib/libcryptominisat5.so")
+    message(STATUS "CryptoMinisat Library: ${CRYPTOMINISAT_LIB}")
+    if (EXISTS ${CRYPTOMINISAT_LIB})
+        message(STATUS "CryptoMinisat Found")
+        add_library(cms STATIC IMPORTED)
+        set_target_properties(cms PROPERTIES IMPORTED_LOCATION "${CRYPTOMINISAT_LIB}")
+    else()
+        message(STATUS "Injecting External Project for Fetching and Building CryptoMinisat")
+        ExternalProject_Add(cms_external
+            #GIT_REPOSITORY git@github.com:msoos/cryptominisat.git
+            GIT_REPOSITORY git@github.com:ipasir2/cryptominisat.git
+            GIT_TAG master
+            PREFIX solvers
+            INSTALL_COMMAND ""
+            BUILD_BYPRODUCTS "${CRYPTOMINISAT_LIB}"
+        )
+        add_library(cms STATIC IMPORTED)
+        add_dependencies(cms cms_external)
+        set_target_properties(cms PROPERTIES IMPORTED_LOCATION "${CRYPTOMINISAT_LIB}")
+    endif()
+endfunction()
